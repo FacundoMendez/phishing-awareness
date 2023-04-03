336216ed8dc6e759f276bba5bebf89b165a036e8
Julian Bellafronte <71682040+Julianbellafronte@users.noreply.github.com>
2023-04-03 19:34:27 -0300

    Update emailData.js

--- src/assets/emailData/emailData.js
+++ src/assets/emailData/emailData.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
 export default function emailData (){
-
+    
+    var numero = Math.floor(Math.random() * (100000 - 0 + 1)) + 0
+    
     var data = {
         service_id: 'service_fv51ia9',
         template_id: 'template_3663h3f',
         user_id: 'aD2ScZKgaRSUBju0w',
         template_params: {
-            'username': '',
+            'id': numero,
             'g-recaptcha-response': ''
         }
     };
