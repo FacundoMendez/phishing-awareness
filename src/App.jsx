5442f2bf4faf324481437261f855b17720f942cd
facundo Mendez <facundo.mendez@live.com.ar>
2023-04-03 11:44:31 -0300

    remove locomotiveScroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 import './App.css'
-import { useEffect, useRef , lazy,Suspense } from 'react';
-import LocomotiveScroll from 'locomotive-scroll';
+import {lazy,Suspense } from 'react';
 const Home = lazy(() => import('./components/home/Home'))
 const Info = lazy(() => import('./components/Info/Info'))
 const Footer = lazy(() => import('./components/footer/Footer'))
 
 
 function App() {
-  const scrollRef = useRef(null);
-
-  useEffect(() => {
-
-      if (scrollRef.current) {
-        new LocomotiveScroll({
-          el: scrollRef.current,
-          inertia: 0.8,
-          smooth: true,
-          getDirection: true,
-        });
-      }
-  }, []);
-
   return (
     <Suspense fallback={[]}>
-      <div className="App" ref={scrollRef} >
+      <div className="App">
           <Home/>
           <Info/>
           <Footer/>
