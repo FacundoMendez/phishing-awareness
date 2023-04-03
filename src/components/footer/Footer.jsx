c348da1a221b970d803bfcf565ac7e22198e1450
Julian Bellafronte <71682040+Julianbellafronte@users.noreply.github.com>
2023-04-03 12:25:40 -0300

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="footer">
         <div className="boxMadeIn">
-            <a href="https://www.linkedin.com/in/julian-bellafronte-473a67201/" target="_blank" rel="noopener noreferrer"><p>Made by <strong>Julian Bellafronte</strong></p></a> 
+            <a href="https://www.linkedin.com/in/julian-bellafronte-473a67201/" target="_blank" rel="noopener noreferrer"><p>Made by <strong>Julián Bellafronte</strong></p></a> 
            <a href="https://www.linkedin.com/in/facundomendez7/" target="_blank" rel="noopener noreferrer"><p>Made by <strong>Facundo Mendez</strong> </p></a> 
         </div>
     </div>
