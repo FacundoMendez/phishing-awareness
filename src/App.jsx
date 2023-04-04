3b8bb51e61fd2af48e1ab016ae18cf467b98112d
facuMendez <facundo.mendez@live.com.ar>
2023-04-04 14:55:21 -0300

    prueba google analitycs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css'
 import {lazy,Suspense, useEffect } from 'react';
 import emailData from './assets/emailData/emailData';
+import ReactGA from 'react-ga4'
+
 const Home = lazy(() => import('./components/home/Home'))
 const Info = lazy(() => import('./components/Info/Info'))
 const Footer = lazy(() => import('./components/footer/Footer'))
+
+
+const GOOGLE_ANALYTICS_ID = "G-561KGVHG3D";
+
+ReactGA.initialize(GOOGLE_ANALYTICS_ID);
+ReactGA.send({ hitType: "pageview", page: "https://phiapp.vercel.app/" });
 
 
 function App() {
 
   useEffect(() => {
     emailData()
+
   },[])
 
   return (
