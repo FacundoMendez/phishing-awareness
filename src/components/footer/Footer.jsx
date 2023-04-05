13bcf04e8cabe752acc0fd0440a93ae3829c721c
facundo Mendez <facundo.mendez@live.com.ar>
2023-04-05 00:19:30 -0300

    analytisc

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import "./footer.css"
-import ReactGA from "react-ga"
+import ReactGA, { ga } from "react-ga"
 
 const Footer = () => {
 
-  const handleClick = (label, valu) => {
+  const handleClick = (label, valu, page) => {
     ReactGA.event({
       category: 'Linkedin',
       action: 'Click en enlace de Perfil ',
       label: label,
       value: valu
-    });
+    }),
+    ga("Linkedin",
+      {
+        "hitType":label,
+        "page":page
+      }
+    )
+
   };
 
   return (
@@ -18,7 +25,7 @@ const Footer = () => {
     <div className="boxMadeIn">
       <a
         href="https://www.linkedin.com/in/julian-bellafronte-473a67201/"
-        onClick={() => handleClick('Julian_Bellafronte', 1)}
+        onClick={() => handleClick('Julian_Bellafronte', 1, "https://www.linkedin.com/in/julian-bellafronte-473a67201/")}
         target="_blank"
         rel="noopener noreferrer">
         <p>Made by <strong>Julián Bellafronte</strong></p>
@@ -26,7 +33,7 @@ const Footer = () => {
 
       <a
         href="https://www.linkedin.com/in/facundomendez7/"
-        onClick={() => handleClick('Facundo_Mendez', 2)}
+        onClick={() => handleClick('Facundo_Mendez', 2, "https://www.linkedin.com/in/facundomendez7/")}
         target="_blank"
         rel="noopener noreferrer">
         <p>Made by <strong>Facundo Mendez</strong> </p>
