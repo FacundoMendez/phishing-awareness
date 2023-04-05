bef028a8a88dedfeaa6c0e6feacb88fc985c33d7
facundo Mendez <facundo.mendez@live.com.ar>
2023-04-05 11:00:52 -0300

    analytics

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,37 +1,32 @@
 import React from 'react'
 import "./footer.css"
-import ReactGA, { ga } from "react-ga"
+import { ga } from "react-ga"
 
 const Footer = () => {
 
   const handleClick = (label, valu) => {
-    ReactGA.event({
-      category: 'Linkedin',
-      action: 'Click en enlace de Perfil ',
-      label: label,
-      value: valu
-    })
+    ga('send', 'event', 'Linkedin', label, "linkedin_link", valu);
   };
 
   return (
     <div className="footer">
-    <div className="boxMadeIn">
-      <a
-        href="https://www.linkedin.com/in/julian-bellafronte-473a67201/"
-        onClick={() => handleClick('Julian_Bellafronte', 1)}
-        target="_blank"
-        rel="noopener noreferrer">
-        <p>Made by <strong>Julián Bellafronte</strong></p>
-      </a>
+      <div className="boxMadeIn">
+        <a
+          href="https://www.linkedin.com/in/julian-bellafronte-473a67201/"
+          onClick={() => handleClick('Julian_Bellafronte', 1)}
+          target="_blank"
+          rel="noopener noreferrer">
+          <p>Made by <strong>Julián Bellafronte</strong></p>
+        </a>
 
-      <a
-        href="https://www.linkedin.com/in/facundomendez7/"
-        onClick={() => handleClick('Facundo_Mendez', 2)}
-        target="_blank"
-        rel="noopener noreferrer">
-        <p>Made by <strong>Facundo Mendez</strong> </p>
-      </a>
-    </div>
+        <a
+          href="https://www.linkedin.com/in/facundomendez7/"
+          onClick={() => handleClick('Facundo_Mendez', 2)}
+          target="_blank"
+          rel="noopener noreferrer">
+          <p>Made by <strong>Facundo Mendez</strong> </p>
+        </a>
+      </div>
     </div>
   )
 }
