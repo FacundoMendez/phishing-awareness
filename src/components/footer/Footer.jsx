76f5e2b16a092e638c373d5e6a7322186ddeb340
facundo Mendez <facundo.mendez@live.com.ar>
2023-04-04 23:39:20 -0300

    analytics

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,12 +4,12 @@ import ReactGA from "react-ga"
 
 const Footer = () => {
 
-  const handleClick = (label, url) => {
+  const handleClick = (label, valu) => {
     ReactGA.event({
       category: 'Linkedin',
       action: 'Click en enlace de Perfil ',
       label: label,
-      value: url
+      value: valu
     });
   };
 
@@ -18,7 +18,7 @@ const Footer = () => {
     <div className="boxMadeIn">
       <a
         href="https://www.linkedin.com/in/julian-bellafronte-473a67201/"
-        onClick={() => handleClick('Julian-Bellafronte', 'https://www.linkedin.com/in/julian-bellafronte-473a67201/')}
+        onClick={() => handleClick('Julian_Bellafronte', 1)}
         target="_blank"
         rel="noopener noreferrer">
         <p>Made by <strong>Julián Bellafronte</strong></p>
@@ -26,7 +26,7 @@ const Footer = () => {
 
       <a
         href="https://www.linkedin.com/in/facundomendez7/"
-        onClick={() => handleClick('Facundo-Mendez', 'https://www.linkedin.com/in/facundomendez7/')}
+        onClick={() => handleClick('Facundo_Mendez', 2)}
         target="_blank"
         rel="noopener noreferrer">
         <p>Made by <strong>Facundo Mendez</strong> </p>
